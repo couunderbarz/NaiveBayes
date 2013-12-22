84bfd7501ea7c5fc5639e058210242132f82ee2e
coffee <koujiyoshida0816@gmail.com>
2013-12-22 19:02:39 +0900

    add nb.py

---
+++ nb.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import math
+from collections import defaultdict
+
+
+class NB(object):
+    """
+    nb = NB()
+    training_data = [(1, {'a': 2, 'b': 2}), (2, {'a': 1, 'c': 4})]
+    nb.train(training_data)
+    testing_data = {'a': 1, 'b': 2}
+    result = nb.train(testing_data)
+    """
+
+    def __init__(self):
+        """
+        """
+        self.all_categories = set()
+        self.category_word_count = defaultdict(lambda: defaultdict(int))
+        self.category_probability = defaultdict(float)
+        self.denominators = defaultdict(int)
+
+    def train(self, data):
+        """dataを用いて分類器を学習する
+        """
+        all_words = set()
+        category_count = defaultdict(int)
+        for category, document in data:
+            self.all_categories.add(category)
+            category_count[category] += 1
+            for word, count in document.iteritems():
+                all_words.add(word)
+                self.category_word_count[category][word] += count
+        for category in self.all_categories:
+            self.denominators[category] = sum(self.category_word_count[category].itervalues()) + len(all_words)
+            self.category_probability[category] = 1.0 * category_count[category] / len(data)
+
+    def classify(self, document):
+        """documentが分類されるcategoryを返す
+        """
+        scores = {category: self._calc_score(document, category)
+                  for category in self.all_categories}
+        best = max(scores, key=scores.get)
+        return best
+
+    def _calc_score(self, document, category):
+        """documentがcategoryに属するスコアを算出する
+        """
+        score = math.log(self.category_probability[category])
+        for word, count in document.iteritems():
+            probability = self._calc_category_word_probability(word, category)
+            score += count * math.log(probability)
+        return score
+
+    def _calc_category_word_probability(self, word, category):
+        """P(word|category)を算出する
+        """
+        numerator = self.category_word_count[category][word] + 1.0
+        return 1.0 * numerator / self.denominators[category]
+
+
+if __name__ == '__main__':
+    training_data = [('中国', {'Chinese': 2, 'Beijing': 1}),
+                     ('中国', {'Chinese': 2, 'Shanghai': 1}),
+                     ('中国', {'Chinese': 1, 'Macao': 1}),
+                     ('日本', {'Tokyo': 1, 'Japan': 1, 'Chinese': 1})]
+    nb = NB()
+    nb.train(training_data)
+    testing_data1 = {'Chinese': 3, 'Tokyo': 1, 'Japan': 1}
+    testing_data2 = {'Sapporo': 3, 'Tokyo': 1}
+    print testing_data1, nb.classify(testing_data1)
+    print testing_data2, nb.classify(testing_data2)
